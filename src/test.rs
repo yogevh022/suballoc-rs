ebfeef324c630ecb24809c7d474235c368195ab3
yogevh022 <yogevh022@gmail.com>
2025-09-18 23:45:40 +0300

    rougher benchmark

--- src/test.rs
+++ src/test.rs
@@ -15,29 +15,18 @@ macro_rules! hotloop {
 
 pub(crate) fn test_suballoc_de() {
     const CAPACITY: usize = 30_000_000;
-    const LOOPS: usize = 1_000_000;
+    const LOOPS: usize = 2_000_000;
     let mut suballoc = SubAllocator::new(CAPACITY);
     let mut allocs = Vec::default();
     let mut rng = rand::rng();
 
     let time = hotloop!(LOOPS; i; {
         let p: f32 = rng.random();
-        if p > 0.45 {
-            let size = rng.random_range(1..2) as usize;
+        if p > 0.49 {
+            let size = rng.random_range(1..=496*1) as usize;
             let alloc = suballoc.allocate(size).unwrap();
-            // println!("allocated: {}", alloc);
             allocs.push(alloc);
         } else if !allocs.is_empty() {
-            // println!("ind: {:?}", &suballoc.free_blocks_indices);
-            // println!(
-            //     "frb: {:?}",
-            //     &suballoc
-            //         .free_blocks
-            //         .iter()
-            //         .enumerate()
-            //         .filter_map(|(i, b)| b.map(|b| (i, b)))
-            //         .collect::<Vec<_>>()
-            // );
             let alloc = allocs.swap_remove(rng.random_range(0..allocs.len()));
             suballoc.deallocate(alloc);
         }
@@ -49,16 +38,6 @@ pub(crate) fn test_suballoc_de() {
         suballoc.used(),
         suballoc.fragment_count()
     );
-    // println!("ind: {:?}", &suballoc.free_blocks_indices);
-    // println!(
-    //     "frb: {:?}",
-    //     &suballoc
-    //         .free_blocks
-    //         .iter()
-    //         .enumerate()
-    //         .filter_map(|(i, b)| b.map(|b| (i, b)))
-    //         .collect::<Vec<_>>()
-    // );
 }
 
 pub(crate) fn test_suballoc() {
